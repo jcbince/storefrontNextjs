1e9468e6f42b7874e565b9fb2f814dc2b9c1fbf9
JC Bince <jbince1@nait.ca>
2022-04-20 17:05:33 -0600

    using other database and got the buttons working, using git before messing this up

---
+++ components/ProductCard/ProductCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image'
+import {productCard, price, name, description} from './styles.module.scss'
+
+
+const myLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`
+}
+
+
+function ProductCard ({children, product, ...props})  {
+  const {productName, productPrice, productDescription, imageUrl, uid}= {...product}
+  return (
+       <aside className={productCard}>
+         <header>
+           <Image
+              loader={myLoader}
+              src={imageUrl}
+              alt={productName}
+              width={418}
+              height={240}
+              quality={50}
+              layout="intrinsic"
+           />
+        
+         </header>
+          <h2 className={name}>{productName}</h2>
+          <p className={price}>${productPrice}</p>
+          <p className={description}>{productDescription}</p>
+          <footer>
+            <form action="#" method="POST">
+              <input type="hidden" name="uid" value={uid}/>
+              <button type="submit">Buy Now</button>
+            </form>
+          </footer>
+       </aside>
+  )
+}
+
+export default ProductCard
