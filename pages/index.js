7e2155fc172305ae0fb196c825ddd8749dfdad50
JC Bince <jbince1@nait.ca>
2022-04-22 17:48:53 -0600

    added header

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,20 @@ export default function Home(props) {
 
   return (
     <>
-      <Head>
-        
-      </Head>
-      <PageTitle title="StoreFront Title" tagline="Featured Products"/>
-      
-      <main>
-      {
-        products.map(product=> <ProductCard  key={product.uid} product={product}/>)
-      }
-      </main>
+        <Head>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+         <meta name="description" content="storefront online running shoes with free shipping"/>
+         <meta name="keywords" content="Shoes, Runing, Running Shoes, Nike Shoes, New Balance Shoes"/>
+           <title>Storefront</title>
+        </Head>
+         <PageTitle tagline="product specials" title="Storefront"/>
+         <main>
+             {  products.map(product=> <ProductCard  key={product.uid} product={product}/>)}
+         </main>
     </>
   )
 
     }
-
-
-
- 
 
 export async function getStaticProps(){
   
