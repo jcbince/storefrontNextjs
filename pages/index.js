1eb80b9ede1e9b9be3783df8211014b52bd511c1
JC Bince <jbince1@nait.ca>
2022-04-22 16:51:05 -0600

    couldnt get my user json placeholder to work. So I will attempt with Jim code

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,46 @@
-import React, {useEffect, useState} from 'react';
-
-
-import Button from "../components/Button/Button";
+import Head from 'next/head'
 import PageTitle from "../components/PageTitle/PageTitle";
-import { User } from "../components/User"
+import ProductCard from "../components/ProductCard/ProductCard";
 
 
-// https://auth-dash-dev-d53c8-default-rtdb.firebaseio.com/products.json - RLTB
+ 
+ 
+export default function Home(props) {
 
-
-
-export default function Home(...props) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [userData, setUserData] = useState([])
-
-  console.log("Home Function rendered")
-  const userProps = props.data;
-  console.log(userProps)
-
-  useEffect(()=>{
-    console.log("Data Loader")
-    async function loadExternalDataTheCRAWay(){
-       const results = await fetch('https://jsonplaceholder.typicode.com/users');
-       
-       const data = await results.json(); 
-    
-       setUserData(data)
-       console.log('data loaded')
-    }
-    loadExternalDataTheCRAWay()
-
-  },[])
+   
+//https://jsonplaceholder.typicode.com/users https://auth-dash-dev-d53c8-default-rtdb.firebaseio.com/products.json - RLTB
+  
+  const products = props.products;
 
   return (
     <>
       <PageTitle title="StoreFront Title" tagline="Featured Users"/>
-      <div style={{textAlign:"center"}}>
-          <Button onClick={()=>setIsLoading(!isLoading)}style={{background:"red"}}>Get Some Data</Button><Button onClick={()=>setIsLoading(!isLoading)} style={{background:"blue"}}>Click Ouput Button</Button>
-          {
-            isLoading&& <p style={{padding:"1rem"}}>This is my output and here is the data {
-              userData.map(({id, name, email, username})=> <User key={id} name={name} email={email} username={username}/>)
-            }</p> 
-          }
- 
-       
-          
-      </div>
+      
       <main>
       {
-        userData.map(({id, name, email, username})=> <User key={id} name={name} email={email} username={username}/>)
+        products.map(product=> <ProductCard product={product}/>)
       }
       </main>
     </>
   )
 
+    }
+
+
+
+ 
+
+export async function getStaticProps(){
+  
+    const res = await fetch('https://shoeshine8k-default-rtdb.firebaseio.com/products.json')
+    const productData = await res.json();
+    const products = Object.values(productData)
+    return {
+          props:{
+              products
+          },
+          revalidate: 60,
+    }
 }
 
-
-// creates terminal list data
-
-// export async function getStaticProps() {
-//   const results = await fetch('https://jsonplaceholder.typicode.com/users');
-  
-//   const userData = await results.json(); 
-//   console.log(userData)
-//   const user = Object.values(userData);
-
-
-//   return {
-//     props:{
-//       user
-//     }
-//   }
-// }
+ 
