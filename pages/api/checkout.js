da0be91a9c5b5e30eee5e777ce8c98ae11206646
JC Bince <jbince1@nait.ca>
2022-04-22 18:16:58 -0600

    added the checkout js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method == "POST") {
+    console.log(req.body.uid);
+    res.status(200).send(`Post Request Product UID: ${req.body.uid}`);
+  } else {
+    res.status(200).send(`not a post request`);
+  }
 }
