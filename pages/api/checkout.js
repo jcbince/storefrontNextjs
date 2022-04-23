16d5e3cfca8b6c595f4a321cec501d6660126d4d
JC Bince <jbince1@nait.ca>
2022-04-22 19:41:21 -0600

    set the stripe pay page up, adjust the price to show. Tested and works

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
               name:product.productName,
               description: product.productDescription,
               images:[product.imageUrl],
-              amount: 15936,
+              amount: product.productPrice*100,
               currency:"CAD",
               quantity:1
   
